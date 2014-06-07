1a40c6fa4cf6d653edf743617069d0d9277225f3
Abhay Rana <capt.n3m0@gmail.com>
2014-06-07 21:03:54 +0530

    Only redirect if we are on 80

--- localhost.js
+++ localhost.js
@@ -1,2 +1,5 @@
-var newLocation =  "https://me.captnemo.in"+document.location.pathname+document.location.search+document.location.hash;
-window.location.replace(newLocation);
+var newLocation =  "https://me.captnemo.in:"+document.location.pathname+document.location.search+document.location.hash;
+//Only redirect if we are on 80
+if(document.location.port===""){
+	window.location.replace(newLocation);
+}
