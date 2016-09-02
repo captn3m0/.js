b72fdcec1c17b5987e70885ade370298d7f7f493
Abhay Rana <me@captnemo.in>
2016-09-02 14:19:04 +0530

    [slack] prevent slack-redir.net
    - Code from https://github.com/jdanyow/slack-direct

---
+++ .slack.com.js
@@ -0,0 +1,15 @@
+// Code from https://github.com/jdanyow/slack-direct
+// UNDER MIT LICENSE
+
+function removeRedirects() {
+  var links = document.querySelectorAll('a[onclick^=this\\.href\\=\\"https\\:\\/\\/slack-redir]'),
+      link,
+      i = links.length;
+  while (i--) {
+    link = links[i];
+    link.removeAttribute('onclick');
+    link.parentNode.replaceChild(link.cloneNode(true), link);
+  }
+}
+
+setInterval(removeRedirects, 300);
