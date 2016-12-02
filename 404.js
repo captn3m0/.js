c77eaf53550ece51b48c17421493d98c9f9942ac
Abhay Rana <me@captnemo.in>
2016-12-02 15:45:00 +0530

    Adds a word-replacing script (RazorPay -> Razorpay)

--- 404.js
+++ 404.js
@@ -88,4 +88,92 @@
     }
     Object.defineProperties(window, {fuckAdBlock : { value: fuck, enumerable: true, writable: false }});
     Object.defineProperties(window, {blockAdBlock : { value: fuck, enumerable: true, writable: false }});
+
+    // Following code comes from http://userscripts-mirror.org/scripts/show/41369
+    // License: http://creativecommons.org/licenses/by-nc-nd/3.0/us/
+
+    (function () {
+
+
+        /*
+            NOTE: 
+                You can use \\* to match actual asterisks instead of using it as a wildcard!
+                The examples below show a wildcard in use and a regular asterisk replacement.
+        */
+
+        var words = {
+        ///////////////////////////////////////////////////////
+
+
+            // Syntax: 'Search word' : 'Replace word',
+            'your a' : 'you\'re a',
+            'imo' : 'in my opinion',
+            'im\\*o' : 'matching an asterisk, not a wildcard',
+            '/\\bD\\b/g' : '[D]',
+            'RazorPay': 'Razorpay'
+
+
+        ///////////////////////////////////////////////////////
+        };
+
+        //////////////////////////////////////////////////////////////////////////////
+        // This is where the real code is
+        // Don't edit below this
+        //////////////////////////////////////////////////////////////////////////////
+
+        var regexs = [], replacements = [],
+            tagsWhitelist = ['PRE', 'BLOCKQUOTE', 'CODE', 'INPUT', 'BUTTON', 'TEXTAREA'],
+            rIsRegexp = /^\/(.+)\/([gim]+)?$/,
+            word, text, texts, i, userRegexp;
+
+        // prepareRegex by JoeSimmons
+        // used to take a string and ready it for use in new RegExp()
+        function prepareRegex(string) {
+            return string.replace(/([\[\]\^\&\$\.\(\)\?\/\\\+\{\}\|])/g, '\\$1');
+        }
+
+        // function to decide whether a parent tag will have its text replaced or not
+        function isTagOk(tag) {
+            return tagsWhitelist.indexOf(tag) === -1;
+        }
+
+        delete words['']; // so the user can add each entry ending with a comma,
+                          // I put an extra empty key/value pair in the object.
+                          // so we need to remove it before continuing
+
+        // convert the 'words' JSON object to an Array
+        for (word in words) {
+            if ( typeof word === 'string' && words.hasOwnProperty(word) ) {
+                userRegexp = word.match(rIsRegexp);
+
+                // add the search/needle/query
+                if (userRegexp) {
+                    regexs.push(
+                        new RegExp(userRegexp[1], 'g')
+                    );
+                } else {
+                    regexs.push(
+                        new RegExp(prepareRegex(word).replace(/\\?\*/g, function (fullMatch) {
+                            return fullMatch === '\\*' ? '*' : '[^ ]*';
+                        }), 'g')
+                    );
+                }
+
+                // add the replacement
+                replacements.push( words[word] );
+            }
+        }
+
+        // do the replacement
+        texts = document.evaluate('//body//text()[ normalize-space(.) != "" ]', document, null, 6, null);
+        for (i = 0; text = texts.snapshotItem(i); i += 1) {
+            if ( isTagOk(text.parentNode.tagName) ) {
+                regexs.forEach(function (value, index) {
+                    console.log(text.data);
+                    text.data = text.data.replace( value, replacements[index] );
+                });
+            }
+        }
+
+    }());
 })(window);
