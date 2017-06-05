a38b296a7407b4bb88a9c3948e64ba0400f6e8d9
Nemo <me@captnemo.in>
2017-06-05 16:35:57 +0530

    Attempting Ctrl+Enter to submit forms

--- mobile.twitter.com.js
+++ mobile.twitter.com.js
@@ -1,6 +1,6 @@
 var updateCache = function() {
   $('a[href^="https://t.co/"] span[aria-hidden=true]').each(function(e, v) {
-    var longlink  = $(v).text().match(/\(link: (.*?)\)/)[1];
+    var longlink = $(v).text().match(/\(link: (.*?)\)/)[1];
     var shortlink = $(v).parent().attr('href');
 
     localStorage.setItem(shortlink, longlink);
@@ -20,5 +20,16 @@ var fixLinks = function() {
   $('[href^="https://t.co/"]').each(fixLink);
 };
 
+var ctrlEnter = function() {
+  $('form').on('keyup', function(e) {
+    if (
+      e.keycode === 13 && (e.ControlLeft === true || e.ControlRight === true)
+    ) {
+      alert('ctrl+enter pressed');
+    }
+  });
+};
+
 setInterval(updateCache, 1000);
 setInterval(fixLinks, 1000);
+setInterval(ctrlEnter, 1000);
